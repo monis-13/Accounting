77105703776f14def6066c7bddcefecb388b6bda
it.dev18 <it.dev18@dawateislami.net>
2020-05-13 01:39:49 +0500

    Finalized CRUD

--- App.js
+++ App.js
@@ -21,33 +21,4 @@ app.use(router);
 
 app.listen(PORT, () => {
     console.log(`Server Started At Port ${PORT}`.green.bold);
-})
-
-
-
-
-
-function errorHandlerTester (a) {
-    const success = a === 5 ? true : false ;
-    return new Promise((resolve , reject) =>
-    {
-      if(!success){
-          resolve("ok");
-      }
-      else{
-          reject("error Occured");
-      }
-    })
-}
-
-const data = {
-    "status": "success",
-    "data": {
-        "n": 0,
-        "opTime": {
-            "ts": "6826056802740731905",
-            "t": 4
-        }
-    }
-};
-console.log(data.data.n);
+});
