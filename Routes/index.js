e1b26f75e8005eb396f4ff94551ad942b62b5c4c
Anas <anas.mazhar1994@gmail.com>
2021-08-07 01:52:13 +0500

    [CA-2] just a change

--- Routes/index.js
+++ Routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mainRouter = express.Router();
 
+// todo
 
 mainRouter.use('/accounts',require('./AccountsRoute'));
 mainRouter.use('/reports',require('./GeneralJournalRoutes'));
